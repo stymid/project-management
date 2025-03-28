f98a0f2235ddcfecb38b00bc885ccea6a39d6fef
Mahdi Sabahi <mrmahdisabahi1@gmail.com>
2025-03-28 12:49:16 +0330

    Add favorite filter functionality with checkbox
    - Implemented filter for displaying only favorite projects
    - Added checkbox with label and styling for user-friendly UI
    - Connected filter functionality to localStorage for favorites

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@
 import ProjectCard from "@/components/ProjectCard";
 import { useProjects } from "@/context/ProjectsContext";
 import { getProjects } from "@/lib/supabase/getProjects";
-import { useEffect } from "react";
+import { Project } from "@/types/project";
+import { useEffect, useState } from "react";
 
 export default function HomePage() {
   const { projects, setProjects } = useProjects();
-
+  const [isFavoriteFilterActive, setIsFavoriteFilterActive] =
+    useState<boolean>(false);
   useEffect(() => {
     const fetchData = async () => {
       const data = await getProjects({});
@@ -15,14 +17,36 @@ export default function HomePage() {
     fetchData();
   }, [setProjects]);
 
+  const filterProjectsByFavorites: () => Project[] = () => {
+    if (!isFavoriteFilterActive) return projects;
+    return projects.filter((p) =>
+      JSON.parse(localStorage.getItem("favorites") || "[]").includes(p.id)
+    );
+  };
+  const filteredProjectsByFavorite = filterProjectsByFavorites();
   return (
     <>
+      <div className="flex gap-3.5">
+        <input
+          type="checkbox"
+          id="favoriteCheckBox"
+          checked={isFavoriteFilterActive}
+          onChange={(e) => setIsFavoriteFilterActive(e.target.checked)}
+          className="h-5 w-5 accent-blue-500 focus:ring-2 focus:ring-blue-300 rounded"
+        />
+        <label
+          htmlFor="favoriteCheckBox"
+          className="text-lg font-medium text-gray-700"
+        >
+          فقط پروژه‌های علاقه‌مندی‌ها
+        </label>
+      </div>
       <h1 className="text-2xl font-bold mb-4">پروژه ها</h1>
       <ul
         className="flex flex-col gap-5
       "
       >
-        {projects.map((project) => (
+        {filteredProjectsByFavorite.map((project) => (
           <li key={project.id}>
             <ProjectCard project={project} />
           </li>
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -3,12 +3,16 @@
 import { useModal } from "@/context/ModalContext";
 
 const Modal: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const { isModalOpen } = useModal();
+  const { isModalOpen, toggleModal } = useModal();
 
   return (
     isModalOpen && (
       <div className="fixed inset-0 flex items-center justify-center bg-gray-500/55 z-50 md:hidden">
-        <div className="bg-white p-6 rounded-md shadow-lg max-w-sm w-full">
+        <div className="bg-white p-6 rounded-md shadow-lg max-w-sm w-full relative">
+          <span className="absolute left-5">
+            <button onClick={() => toggleModal()}>x</button>
+          </span>
+
           {children}
         </div>
       </div>
